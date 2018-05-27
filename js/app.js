05301c7425163c1f1f5f00f232ded0fb47e133b9
Mohamed Abbas <mohamedabbas92a@gmail.com>
2018-05-27 11:26:54 +0200

    Feat: Add responsiveness to work across all devices

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 const cards= document.querySelectorAll('.deck .card'); //output a NodeList
 const cardsArr= [...cards]; //convert the NodeList to Array
 const deck= document.querySelector('.deck');
@@ -179,15 +168,3 @@ setInterval(timer, 1000); //showing timer that count time every 1 second
 shuffleDeck();
 restart.addEventListener('click',restarting);
 deck.addEventListener('click', flipping);
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
